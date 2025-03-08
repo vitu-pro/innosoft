30d55c2f14d3a0f42d46366c96388a96358e9277
vitu-pro <vitumbikonyambose@gmail.com>
2025-03-08 07:59:42 +0200

    install react js and tailwind css and create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-light text-teal-950">
+        Welcome Inno Soft!
+      </h1>
+    </>
+  )
+}
+
+export default App
